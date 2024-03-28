6272942923413b9b492d3c30f5a68892e7c00c8b
jinoogi <163394990+jinoogi@users.noreply.github.com>
2024-03-28 11:39:14 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,23 +1,18 @@
-def single_digit_checker(num): #숫자를 입력받아 2자리수이상이면 그냥 return, 1자릿수이면 2자릿수로 변환해서 반환
-    if len(num) <2:
-        return "0"+num
-    elif len(num) >=2:
-        return num
-    
-def first_digit_selector(num): #숫자를 입력받아 1의자릿수를 반환
-    if len(num) <2:
-        return num
-    elif len(num) >=2:
-        return num[1]
+num = int(input())
+result = num
 
-count=0
-N=input() #N을 입력받음
-num=single_digit_checker(N)
+cycle = 0
+
 while True:
-    num=single_digit_checker(num)
-    sum=str(int(num[0])+int(num[1]))
-    num=num[1]+first_digit_selector(sum)
-    count=count+1
-    if int(N)==int(num):
+    cycle +=1
+    if num >= 10 :
+        num = (num//10 + num%10)%10+(num%10)*10
+    else :
+        num = num*10+num
+
+
+    if num == result:
         break
-print(count)
+
+
+print(cycle)
